25d92e1cf0a436a86b8dc527a60a8a676a442480
nrobledosagredo <nrobledosagredo@gmail.com>
2024-01-27 13:10:26 -0300

    added firebase, theme-provider and mode-toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
 import Home from "@/pages/Home"
+import Login from "@/pages/Login"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+
+import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   )
 }
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button"
+import { ModeToggle } from "@/components/mode-toggle"
 
-export default function Home() {
+function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <ModeToggle />
     </div>
   )
 }
+export default Home
