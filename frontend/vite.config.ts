e138335dfaadf75d0e4b3891ce54afaa9b6b2794
nrobledosagredo <nrobledosagredo@gmail.com>
2024-01-27 12:27:17 -0300

    added prettier, https and certs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    https: {
+      key: fs.readFileSync(
+        path.resolve(__dirname, "./192.168.159.129+3-key.pem")
+      ),
+      cert: fs.readFileSync(path.resolve(__dirname, "./192.168.159.129+3.pem")),
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
