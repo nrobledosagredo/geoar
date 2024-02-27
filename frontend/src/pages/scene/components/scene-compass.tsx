33fe0ce42f74937bf4ea80c88305e56655d007dd
nrobledosagredo <nrobledosagredo@gmail.com>
2024-02-27 17:25:07 -0300

    compass fix

--- frontend/src/pages/scene/components/scene-compass.tsx
+++ frontend/src/pages/scene/components/scene-compass.tsx
@@ -1,60 +1,72 @@
-import { useEffect, useState } from "react"
-import { ChevronUp } from "lucide-react"
+import { useEffect, useState } from "react";
+import { ChevronUp } from "lucide-react";
 
-const compassPoints = ["S", "SE", "E", "NE", "N", "NW", "W", "SW"]
+const compassPoints = ["S", "SE", "E", "NE", "N", "NW", "W", "SW"];
 
+// Extender la interfaz DeviceOrientationEvent para incluir webkitCompassHeading
 declare global {
+  interface DeviceOrientationEvent {
+    webkitCompassHeading?: number;
+  }
+
   interface Window {
     DeviceOrientationEvent: typeof DeviceOrientationEvent & {
-      requestPermission?: () => Promise<string>
-    }
+      requestPermission?: () => Promise<string>;
+    };
   }
 }
 
 export function SceneCompass() {
-  const [orientation, setOrientation] = useState<number>(0)
+  const [orientation, setOrientation] = useState<number>(0);
 
   useEffect(() => {
     const handleOrientation = (event: Event) => {
-      const orientationEvent = event as DeviceOrientationEvent
-      const { alpha } = orientationEvent
-      if (alpha !== null) {
-        setOrientation(alpha)
+      const orientationEvent = event as DeviceOrientationEvent;
+      let heading = orientationEvent.alpha;
+      if (orientationEvent.webkitCompassHeading) {
+        // iOS
+        heading = orientationEvent.webkitCompassHeading;
+      } else if (orientationEvent.absolute && orientationEvent.alpha) {
+        // Android
+        heading = 360 - orientationEvent.alpha;
       }
-    }
+      if (heading !== null) {
+        setOrientation(heading);
+      }
+    };
 
     const requestOrientationPermission = async () => {
       if (
         typeof window.DeviceOrientationEvent.requestPermission === "function"
       ) {
         const permissionState =
-          await window.DeviceOrientationEvent.requestPermission()
+          await window.DeviceOrientationEvent.requestPermission();
         if (permissionState === "granted") {
-          addOrientationListener()
+          addOrientationListener();
         }
       } else {
-        addOrientationListener()
+        addOrientationListener();
       }
-    }
+    };
 
     const addOrientationListener = () => {
-      const orientationEventType =
+      const orientationEventType: string =
         "ondeviceorientationabsolute" in window
           ? "deviceorientationabsolute"
-          : "deviceorientation"
-      window.addEventListener(
-        orientationEventType as keyof WindowEventMap,
-        handleOrientation
-      )
-    }
+          : "deviceorientation";
+      window.addEventListener(orientationEventType, handleOrientation);
+    };
 
-    requestOrientationPermission()
+    requestOrientationPermission();
 
     return () => {
-      window.removeEventListener("deviceorientation", handleOrientation)
-      window.removeEventListener("deviceorientationabsolute", handleOrientation)
-    }
-  }, [])
+      window.removeEventListener("deviceorientation", handleOrientation);
+      window.removeEventListener(
+        "deviceorientationabsolute",
+        handleOrientation
+      );
+    };
+  }, []);
 
   return (
     <div className="mx-1">
@@ -62,8 +74,8 @@ export function SceneCompass() {
         <div className="relative w-full">
           {compassPoints.map((point, index) => {
             const position =
-              (((index * 45 - orientation + 360) % 360) / 360) * 100
-            const isMainPoint = ["N", "S", "E", "W"].includes(point)
+              (((index * 45 - orientation + 360) % 360) / 360) * 100;
+            const isMainPoint = ["N", "S", "E", "W"].includes(point);
             return (
               <div
                 key={index}
@@ -71,18 +83,24 @@ export function SceneCompass() {
                 style={{ left: `calc(${position}%` }}
               >
                 <p
-                  className={`${isMainPoint ? "font-bold text-lg -mb-0.5 -mt-0.5" : "text-muted-foreground text-xs"}`}
+                  className={`${
+                    isMainPoint
+                      ? "font-bold text-lg -mb-0.5 -mt-0.5"
+                      : "text-muted-foreground text-xs"
+                  }`}
                 >
                   {point}
                 </p>
 
                 <p
-                  className={`text-xs font-bold ${isMainPoint ? "" : "text-muted-foreground"}`}
+                  className={`text-xs font-bold ${
+                    isMainPoint ? "" : "text-muted-foreground"
+                  }`}
                 >
                   |
                 </p>
               </div>
-            )
+            );
           })}
           <ChevronUp
             className="absolute top-1/2 left-1/2 transform -translate-x-1/2 text-primary"
@@ -91,5 +109,5 @@ export function SceneCompass() {
         </div>
       </div>
     </div>
-  )
+  );
 }
