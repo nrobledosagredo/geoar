dea186938f15e780f3064f14fd38597471b68c89
nrobledosagredo <nrobledosagredo@gmail.com>
2024-02-27 18:47:59 -0300

    map tweak

--- frontend/src/pages/scene/components/scene-map.tsx
+++ frontend/src/pages/scene/components/scene-map.tsx
@@ -76,7 +76,7 @@ export function SceneMap({ points, infoCards, trees }: SceneMapProps) {
         zoom={20}
         scrollWheelZoom={false}
         zoomControl={false}
-        className="w-full h-full overflow-hidden"
+        className="w-full h-full rounded-t-full sm:rounded-lg landscape:rounded-lg"
       >
         {/* Capa de OpenStreetMap */}
         <TileLayer url={tileLayerUrl} />
