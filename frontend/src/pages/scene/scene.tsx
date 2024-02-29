21845457f289cb0c34e3c915e3b35edcba0c787e
nrobledosagredo <nrobledosagredo@gmail.com>
2024-02-29 01:56:48 -0300

    Enable SceneMap component in Scene.tsx

--- frontend/src/pages/scene/scene.tsx
+++ frontend/src/pages/scene/scene.tsx
@@ -3,7 +3,7 @@ import { SceneArrow } from "@/pages/scene/components/scene-arrow"
 import { SceneCompass } from "@/pages/scene/components/scene-compass"
 import { SceneInfoCard } from "@/pages/scene/components/scene-infocard"
 import { SceneLoadingScreen } from "@/pages/scene/components/scene-loading-screen"
-//import { SceneMap } from "@/pages/scene/components/scene-map"
+import { SceneMap } from "@/pages/scene/components/scene-map"
 import { SceneNav } from "@/pages/scene/components/scene-nav"
 import { ScenePoint } from "@/pages/scene/components/scene-point"
 import { SceneTreeCard } from "@/pages/scene/components/scene-treecard"
@@ -98,9 +98,7 @@ export function Scene() {
       <div className="relative z-40 pointer-events-none">
         <SceneNav />
         <SceneCompass />
-        {/* 
         <SceneMap points={points} infoCards={infoCards} trees={trees} />
-        */}
       </div>
 
       <a-scene
@@ -115,7 +113,7 @@ export function Scene() {
           //gps-new-camera={`gpsMinDistance: 5; simulateLatitude: ${String(simulateLatitude)}; simulateLongitude: ${String(simulateLongitude)}`}
           gps-new-camera="gpsMinDistance: 5"
           target-finder
-          ios-orientation-fix
+          //ios-orientation-fix
           //far={cameraMaxDistance}
         >
           {/* Flecha 3D que apunta al siguiente punto */}
