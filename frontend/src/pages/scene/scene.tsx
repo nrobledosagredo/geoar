b4a916973f0efb7903fcae82e96fd01e8c24d80a
nrobledosagredo <nrobledosagredo@gmail.com>
2024-02-28 23:11:48 -0300

    test

--- frontend/src/pages/scene/scene.tsx
+++ frontend/src/pages/scene/scene.tsx
@@ -123,8 +123,9 @@ export function Scene() {
       console.log("initialCompassHeading", initialCompassHeading)
       const camera = document.querySelector("a-camera")
       if (camera) {
+        console.log("ANTES: Camera rotation:", camera.getAttribute("rotation"));
         camera.setAttribute("rotation", `0 ${360 - initialCompassHeading} 0`)
-        console.log("initialCompassHeading", initialCompassHeading)
+        console.log("DESPUES: Camera rotation:", camera.getAttribute("rotation"));
       }
     }
   }, [initialCompassHeading])
