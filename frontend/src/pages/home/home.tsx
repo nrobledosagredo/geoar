203a98efee5f89a863f8e258cb9c1e7bff1ff989
nrobledosagredo <nrobledosagredo@gmail.com>
2024-03-06 21:07:37 -0300

    Update sw.js and home.tsx files

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,27 +1,29 @@
 // home.tsx
-import { useEffect } from "react"
-import { ChevronRight } from "lucide-react"
-import { useTranslation } from "react-i18next"
-import { Link } from "react-router-dom"
+import { useEffect } from "react";
+import { ChevronRight } from "lucide-react";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
-import { Button } from "@/components/ui/button"
-import { MainNav } from "@/components/main-nav"
-import { VerticalLogo } from "@/components/vertical-logo"
+import { Button } from "@/components/ui/button";
+import { MainNav } from "@/components/main-nav";
+import { VerticalLogo } from "@/components/vertical-logo";
 
 export function Home() {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   useEffect(() => {
-    document.title = "GeoAR"
-  }, [t])
+    document.title = "GeoAR";
+  }, [t]);
 
   return (
-    <div className="h-screen flex flex-col overflow-hidden">
+    <div className="h-screen flex overflow-hidden relative">
       {/* Barra de navegación */}
-      <MainNav />
+      <div className="absolute w-full">
+        <MainNav />
+      </div>
 
       {/* Contenedor de la página de inicio */}
-      <div className="flex-grow flex flex-col justify-center items-center text-center space-y-8">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center space-y-8">
         {/* Título */}
         <VerticalLogo />
 
@@ -43,5 +45,5 @@ export function Home() {
         </div>
       </div>
     </div>
-  )
+  );
 }
