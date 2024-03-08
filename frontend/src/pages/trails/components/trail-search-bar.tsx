55ed6d67c7b8c2a2eb36bfd9812622b0b47b6469
nrobledosagredo <nrobledosagredo@gmail.com>
2024-03-08 19:13:22 -0300

    Update DrawerContent height in TrailSearchBar component

--- frontend/src/pages/trails/components/trail-search-bar.tsx
+++ frontend/src/pages/trails/components/trail-search-bar.tsx
@@ -57,7 +57,7 @@ export function TrailSearchBar({ onSearch }: TrailSearchProps) {
           {t("search_bar_placeholder")}
         </Button>
       </DrawerTrigger>
-      <DrawerContent className="h-full">
+      <DrawerContent className="h-5/6">
         <DrawerHeader className="pt-2"></DrawerHeader>
         <TrailSearchForm onSearch={onSearch} />
       </DrawerContent>
